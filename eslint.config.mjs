922dbc07b4d7b3ee74a342b00bf9ca2fb9154da1
Schmakus <Schmakus@gmail.com>
2024-09-02 09:39:12 +0200

    change sourceTypew to module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,68 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/.prettierrc.js", "**/.eslintrc.js", "admin/words.js", "lib/utils.js"],
-}, ...compat.extends("eslint:recommended"), {
-    plugins: {},
+export default [
+	{
+		ignores: ['**/.prettierrc.js', '**/.eslintrc.js', 'admin/words.js', 'lib/utils.js'],
+	},
+	...compat.extends('eslint:recommended'),
+	{
+		plugins: {},
 
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.mocha,
-        },
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.mocha,
+			},
 
-        ecmaVersion: 2020,
-        sourceType: "commonjs",
-    },
+			ecmaVersion: 2020,
+			sourceType: 'module',
+		},
 
-    rules: {
-        indent: ["error", "tab", {
-            SwitchCase: 1,
-        }],
+		rules: {
+			indent: [
+				'error',
+				'tab',
+				{
+					SwitchCase: 1,
+				},
+			],
 
-        "no-console": "off",
+			'no-console': 'off',
 
-        "no-unused-vars": ["error", {
-            ignoreRestSiblings: true,
-            argsIgnorePattern: "^_",
-        }],
+			'no-unused-vars': [
+				'error',
+				{
+					ignoreRestSiblings: true,
+					argsIgnorePattern: '^_',
+				},
+			],
 
-        "no-var": "error",
-        "no-trailing-spaces": "error",
-        "prefer-const": "error",
+			'no-var': 'error',
+			'no-trailing-spaces': 'error',
+			'prefer-const': 'error',
 
-        quotes: ["error", "single", {
-            avoidEscape: true,
-            allowTemplateLiterals: true,
-        }],
+			quotes: [
+				'error',
+				'single',
+				{
+					avoidEscape: true,
+					allowTemplateLiterals: true,
+				},
+			],
 
-        semi: ["error", "always"],
-    },
-}];
+			semi: ['error', 'always'],
+		},
+	},
+];
