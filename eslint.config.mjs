9b8b70343e447dd8646ed6127fc6de7e3d4b9a52
Matthias Kleine <github.com9@emailu.de>
2025-03-03 11:51:49 +0100

    @iobroker/eslint-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,71 +1,29 @@
-import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
+// ioBroker eslint template configuration file for js and ts files
+// Please note that esm or react based modules need additional modules loaded.
+import config from '@iobroker/eslint-config';
 
 export default [
-	{
-		ignores: ['**/.prettierrc.js', '**/.eslintrc.js', 'admin/words.js', 'lib/utils.js'],
-	},
-	...compat.extends('eslint:recommended'),
-	{
-		plugins: {},
-
-		languageOptions: {
-			globals: {
-				...globals.node,
-				...globals.mocha,
-			},
-
-			ecmaVersion: 2020,
-			sourceType: 'module',
-		},
-
-		rules: {
-			indent: [
-				'error',
-				'tab',
-				{
-					SwitchCase: 1,
-				},
-			],
-
-			'no-console': 'off',
-
-			'no-unused-vars': [
-				'error',
-				{
-					vars: 'all',
-					args: 'after-used',
-					caughtErrors: 'all',
-					ignoreRestSiblings: false,
-					reportUsedIgnorePattern: false,
-				},
-			],
-
-			'no-var': 'error',
-			'no-trailing-spaces': 'error',
-			'prefer-const': 'error',
-
-			quotes: [
-				'error',
-				'single',
-				{
-					avoidEscape: true,
-					allowTemplateLiterals: true,
-				},
-			],
-
-			semi: ['error', 'always'],
-		},
-	},
+    ...config,
+    {
+        // specify files to exclude from linting here
+        ignores: [
+            '.dev-server/',
+            '.vscode/',
+            '*.test.js', 
+            'test/**/*.js', 
+            '*.config.mjs', 
+            'build', 
+            'admin/build', 
+            'admin/words.js',
+            'admin/admin.d.ts',
+            '**/adapter-config.d.ts'     
+        ] 
+    },
+    {
+        // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
+        // as this improves maintainability. jsdoc warnings will not block buiuld process.
+        rules: {
+            // 'jsdoc/require-jsdoc': 'off',
+        },
+    },
 ];
